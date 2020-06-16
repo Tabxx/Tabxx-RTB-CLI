5032855c5fba0c446e8e16fa1f5446de7f67dd20
yuxinxin <yuxinxin@myhexin.com>
2020-06-16 12:34:02 +0800

    auto commit

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 let str: string = 'Hello RTB CLI!'
-const arr: number[] = [1, 2, 34]
+const arr: number[] = [1, 2, 3, 4, 5]
 arr.map((item) => {
   str += item.toString()
 })
 document.body.innerHTML = str
-
-const env = process.env.NODE_ENV
-console.log(env)
-
-document.write(
-  '<script src="http://' +
-    (location.host || 'localhost').split(':')[0] +
-    ':35729/livereload.js?snipver=1"></' +
-    'script>'
-)
 
 export default str
