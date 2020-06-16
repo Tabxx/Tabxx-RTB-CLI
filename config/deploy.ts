b4a56636f45a6f6c2a4b25fe1e8aa24e5815afc3
yuxinxin <yuxinxin@myhexin.com>
2020-06-16 12:27:09 +0800

    auto commit

--- config/deploy.ts
+++ config/deploy.ts
@@ -1,8 +1,8 @@
 module.exports = {
   host: '49.234.11.77',
   port: 22,
-  username: 'Tab',
-  password: 'yxx970301.',
+  username: 'root',
+  password: 'Yxx970301.Tab.',
   distPath: 'dist', // 本地打包dist目录
   zipName: 'release', // 压缩包名称
   webDir: '/var/www/assets/cli/test', // 正式环境服务器地址
