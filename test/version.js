bc4b157d15f1d946de63b2a18c8315dcdfc03fda
yuxinxin <yuxinxin@myhexin.com>
2020-06-16 11:30:48 +0800

    auto commit

--- test/version.js
+++ test/version.js
@@ -1,8 +1,8 @@
 const shell = require('shelljs')
 const params = JSON.parse(process.env.npm_config_argv).original
-const version = params[2] === '-V' && params[3] || 'patch' // 默认patch更新
+const version = params[3] || 'patch' // 默认patch更新
 
 shell.exec('git add .')
 shell.exec('git commit -m "auto commit"')
 shell.exec(`npm version ${version}`)
-process.exit();
+process.exit()
